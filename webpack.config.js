c01e9c1828c01b945476089c3968364bc515ec04
Martin Nikl <Martin.Nikl@dixonscarphone.com>
2018-10-10 15:43:21 +0200

    app config

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractText = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+function getEnv(env) {
+  try {
+    return env.prod ? 'prod' : 'dev';
+  }
+  catch(err) {
+    console.log('Error is getting env in webpack.config.js.', err);
+
+    return 'dev';
+  }
+}
+
+module.exports = function (env) {
+  console.log('<-- AUTOSKOLA startup... -->');
+
+  var env = getEnv(env);
+
+  var config = {
+    entry: {
+      'app': './src/js/app.js'
+    },
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: '[name].min.js'
+    },
+    mode: env === 'prod' ? 'production' : 'development',
+    devtool: env === 'prod' ? 'cheap-module-source-map' : 'inline-source-map', // viz.: https://webpack.js.org/configuration/devtool/
+    plugins: [
+      new ExtractText({
+        filename: '[name].min.css',
+        disable: false,
+        allChunks: true
+      }),
+      new HtmlWebpackPlugin({
+        template: './www/index.html'
+      })
+    ],
+    resolve: {
+      extensions: ['.js', '.json']
+    },
+    devServer: {
+      contentBase: './dist',
+      compress: true,
+      port: 9000
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          include: path.join(__dirname, '/src/js'),
+          exclude: /node_modules/,
+          query: {
+            cacheDirectory: true,
+            presets: ["@babel/preset-env"]
+          }
+        },
+        {
+          test: /\.(scss)$/,
+          use: ExtractText.extract({
+            "fallback": "style-loader",
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  minimize: env === 'prod' ? true : false
+                }
+              },
+              'sass-loader'
+            ]
+          })
+        }
+      ]
+    }
+  };
+
+  return config;
+}
